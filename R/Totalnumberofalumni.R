2bdc993f37f48c41f27ad18d38b1aea4fcffc1d6
Robert Malongo <rmalongo@colby.edu>
2017-06-26 15:14:46 -0400

    major change to the function

--- R/Totalnumberofalumni.R
+++ R/Totalnumberofalumni.R
@@ -6,7 +6,47 @@
 #' @return A numeric value
 #' @export
 
-Alumnitotal <- function(x, y){
-  A1 <- ifelse(distm(x[,2:3], y,fun=distHaversine)/1609<=20,"TRUE","FALSE")
-  return(sum(A1=="TRUE"))
+Alumnitotal <- function(x,y){
+
+  # define inputs
+
+  tmp_zip <- x
+  tmp_center_zip <- y
+  radius <- 50
+
+  # grab zipcodes from zipcode package
+  data("zipcode")
+
+  # clean zipcode
+
+  tmp_zip <- clean.zipcodes(tmp_zip)
+
+  # input 1 - address
+  # subset zipcode data based on address input
+  tmp <- zipcode[zipcode$zip %in% tmp_zip,]
+  tmp_cord <- data.frame(longitude =tmp$longitude,
+                         latitude =  tmp$latitude)
+
+
+  # input 2 - center point
+  # subset zipcode data based on zipcode centroid input
+  tmp_center <- zipcode[zipcode$zip == tmp_center_zip,]
+  tmp_center <- data.frame(longitude =  tmp_center$longitude,
+                           latitude =  tmp_center$latitude)
+
+  tmp_zip
+  # calculate distance between input and specified zipcode
+  distance <- ifelse(distm(tmp_cord, tmp_center, fun=distHaversine)/1609<=radius,"TRUE","NA")
+
+
+  if(distance==TRUE){
+
+    return(sum==TRUE)
+
+  }else {
+
+    return(NA)
+  }
+
 }
+
